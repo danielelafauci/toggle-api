67a71a044bcd5ad1c40e761e28d8f5d027f69c43
Daniele <daniele.lafauci@gmail.com>
2026-03-13 12:58:35 +0100

    chore: initialize project

---
+++ src/main/java/com/toggleapi/toggleapi/ToggleapiApplication.java
@@ -0,0 +1,13 @@
+package com.toggleapi.toggleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToggleapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToggleapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toggleapi/toggleapi/ToggleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toggleapi.toggleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToggleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
